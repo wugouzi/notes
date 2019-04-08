2b98c3690e6f05e95521cc39ccf54bffb6fc1eec
wu <f_dogs@protonmail.com>
2019-04-08 11:52:39 +0800

    4.8.2019

--- latex/auto/NumericalAnalysis.el
+++ latex/auto/NumericalAnalysis.el
@@ -7,12 +7,12 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -36,22 +36,25 @@
     "mathabx"
     "mathtools"
     "pgfplots")
+   (TeX-add-symbols
+    '("bl" 1))
    (LaTeX-add-labels
-    "sec:org0dbbfd3"
-    "sec:orga8282ef"
-    "sec:org8f72cbe"
-    "sec:orgf114877"
-    "sec:org09037e4"
-    "sec:orgd3125ac"
-    "sec:orgbf2d66f"
-    "sec:org1f94b94"
-    "sec:orge54b9da"
-    "sec:orgfb9d6c4"
-    "sec:orgfc4490e"
-    "sec:org7bf2c0d"
-    "sec:orgea19073"
-    "sec:org74c517b"
-    "sec:org00b9e98")
+    "sec:orgadb8da5"
+    "sec:orgff8f523"
+    "sec:org8186040"
+    "sec:org923f7f9"
+    "sec:orgf37f077"
+    "sec:org312f177"
+    "sec:org1957493"
+    "sec:org466b1fc"
+    "sec:org0000473"
+    "sec:org67a84c6"
+    "sec:org6722136"
+    "sec:org57bca8a"
+    "sec:orga3db150"
+    "sec:org04e29e0"
+    "sec:org8a04579"
+    "sec:org3a1e900")
    (LaTeX-add-amsthm-newtheorems
     "theorem"
     "definition"
