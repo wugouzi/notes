6ef91f623710d5b65f974a8d3a892378f00f508a
wu <f_dogs@protonmail.com>
2019-08-02 18:10:53 +0800

    8.2.2019

---
+++ books/auto/preamble.el
@@ -0,0 +1,54 @@
+(TeX-add-style-hook
+ "preamble"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "dvipsnames") ("tcolorbox" "most")))
+   (TeX-run-style-hooks
+    "xcolor"
+    "forest"
+    "mathtools"
+    "pgfplots"
+    "amsthm"
+    "amsmath"
+    "commath"
+    "amsfonts"
+    "amssymb"
+    "stmaryrd"
+    "empheq"
+    "tikz"
+    "tcolorbox"
+    "actuarialsymbol"
+    "threeparttable"
+    "scalerel"
+    "stackengine"
+    "graphicx")
+   (TeX-add-symbols
+    '("stcomp" 1)
+    '("dom" 1)
+    '("fixwidehat" 1)
+    '("wt" 1)
+    '("Wt" 1)
+    '("bl" 1)
+    '("wbar" 1)
+    '("wtilde" 1)
+    '("what" 1)
+    "widehatsym"
+    "lowerwidehatsym"
+    "varprod"
+    "bigtimes"
+    "odiv"
+    "subsetsim"
+    "simsubset"
+    "simsubsetsim")
+   (LaTeX-add-amsthm-newtheorems
+    "theorem"
+    "definition"
+    "corollary"
+    "lemma"
+    "proposition"
+    "example"
+    "remark"))
+ :latex)
+
