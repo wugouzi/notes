497dfa1a7586d060e0b232129f0bc7ac67c363cb
wu <f_dogs@protonmail.com>
2020-06-10 21:00:07 +0800

    6.10.2020

--- latex/test/auto/preamble.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "preamble"
- (lambda ()
-   (setq TeX-command-extra-options
-         "-shell-escape")
-   (LaTeX-add-environments
-    '("gathered" LaTeX-env-args ["argument"] 0)))
- :plain-tex)
-
