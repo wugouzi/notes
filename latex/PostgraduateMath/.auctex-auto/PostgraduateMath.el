12decd412dc826ac5899ae540a405def8371694a
wu <f_dogs@protonmail.com>
2020-08-30 10:38:26 +0800

    8.30.2020

--- latex/PostgraduateMath/.auctex-auto/PostgraduateMath.el
+++ latex/PostgraduateMath/.auctex-auto/PostgraduateMath.el
@@ -1,12 +1,12 @@
 (TeX-add-style-hook
- "ModalLogic"
+ "PostgraduateMath"
  (lambda ()
    (setq TeX-command-extra-options
          "-shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("ctex" "UTF8")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
@@ -34,22 +34,19 @@
     "amssymb"
     "capt-of"
     "hyperref"
-    "minted")
+    "minted"
+    "ctex")
    (LaTeX-add-labels
-    "sec:org98e621f"
-    "sec:orgb6998a8"
-    "sec:orgdce4592"
-    "sec:org00ed62b"
-    "sec:orga66b40d"
-    "sec:orgc99a402"
-    "sec:org3c788a9"
-    "sec:orge650ae6"
-    "sec:org57ae52d"
-    "sec:org0e6a77f"
-    "thm2.20"
-    "example2.22"
-    "example2.23"
-    "thm2.24"
-    "ex2.2.8"))
+    "sec:orga117e8a"
+    "sec:orga4f7a06"
+    "sec:org16c4bb5"
+    "eq2-2"
+    "eq2-3"
+    "sec:orgd57f581"
+    "sec:org6393676"
+    "sec:org09af4b4"
+    "sec:org9cc2d8d"
+    "sec:orgbb0c199"
+    "sec:org151a53a"))
  :latex)
 
