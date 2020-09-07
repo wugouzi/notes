456c7e1bb34e5c2630654616fdba9284ad0d413b
wu <f_dogs@protonmail.com>
2020-09-07 10:51:13 +0800

    9.7.2020

---
+++ latex/LinearAlgebra/.auctex-auto/LinearAlgebra.el
@@ -0,0 +1,52 @@
+(TeX-add-style-hook
+ "LinearAlgebra"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("ctex" "UTF8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "../preamble"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted"
+    "ctex")
+   (LaTeX-add-labels
+    "sec:org20f8d76"
+    "sec:org28f2c23"
+    "sec:org9407bc6"
+    "sec:org7e86828"
+    "eq1.1"
+    "eq1.2"
+    "eq1.11"
+    "eq1.22"
+    "eq1.1.4.1"
+    "eq1.1.4.2"
+    "sec:orgf2ae0b2"))
+ :latex)
+
