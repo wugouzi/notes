d1a7bfd0dc1a1fc0653a2384798b8f8d0216a9cf
wu <f_dogs@protonmail.com>
2020-09-09 09:17:19 +0800

    9.9.2020

--- latex/LinearAlgebra/.auctex-auto/LinearAlgebra.el
+++ latex/LinearAlgebra/.auctex-auto/LinearAlgebra.el
@@ -37,16 +37,30 @@
     "minted"
     "ctex")
    (LaTeX-add-labels
-    "sec:org20f8d76"
-    "sec:org28f2c23"
-    "sec:org9407bc6"
-    "sec:org7e86828"
+    "sec:orgfc059f3"
+    "sec:orged52cb9"
+    "sec:orgbd9790b"
+    "sec:orgb02a158"
     "eq1.1"
     "eq1.2"
     "eq1.11"
     "eq1.22"
     "eq1.1.4.1"
     "eq1.1.4.2"
-    "sec:orgf2ae0b2"))
+    "sec:org03e621a"
+    "sec:orgd9ed85b"
+    "sec:org320be7f"
+    "sec:org299871b"
+    "sec:org81788c4"
+    "sec:org2e24f82"
+    "eq2.4.3.1"
+    "eq2.4.3.2"
+    "sec:orgee80af7"
+    "sec:org217ed4c"
+    "sec:org28598e6"
+    "sec:org91fa81a"
+    "sec:orgd5874c7"
+    "sec:orgfe85b7d"
+    "sec:orgb7a44c7"))
  :latex)
 
