28d456d11cd5f1348a024bd6d9e57c841d87fd13
wu <f_dogs@protonmail.com>
2020-09-10 21:32:07 +0800

    9.10.2020

--- latex/LinearAlgebra/.auctex-auto/LinearAlgebra.el
+++ latex/LinearAlgebra/.auctex-auto/LinearAlgebra.el
@@ -8,13 +8,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("ctex" "UTF8")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -37,30 +37,53 @@
     "minted"
     "ctex")
    (LaTeX-add-labels
-    "sec:orgfc059f3"
-    "sec:orged52cb9"
-    "sec:orgbd9790b"
-    "sec:orgb02a158"
+    "sec:org63b65bb"
+    "sec:orgc4c2e42"
+    "sec:orgaaacfe8"
+    "sec:org9e6ccfa"
     "eq1.1"
     "eq1.2"
     "eq1.11"
     "eq1.22"
     "eq1.1.4.1"
     "eq1.1.4.2"
-    "sec:org03e621a"
-    "sec:orgd9ed85b"
-    "sec:org320be7f"
-    "sec:org299871b"
-    "sec:org81788c4"
-    "sec:org2e24f82"
+    "prop2.1.5"
+    "cor2.1.5.2"
+    "sec:orgb72c330"
+    "sec:orgd1bd5d4"
+    "sec:org2054d95"
+    "sec:org66c43f7"
+    "sec:org9bdbc78"
+    "sec:orgf2b7970"
     "eq2.4.3.1"
     "eq2.4.3.2"
-    "sec:orgee80af7"
-    "sec:org217ed4c"
-    "sec:org28598e6"
-    "sec:org91fa81a"
-    "sec:orgd5874c7"
-    "sec:orgfe85b7d"
-    "sec:orgb7a44c7"))
+    "sec:org99f8dd0"
+    "sec:org9f751ee"
+    "sec:org4e8d274"
+    "sec:org2d20245"
+    "sec:org704d09e"
+    "sec:org857f9ad"
+    "sec:org7432817"
+    "sec:org57a4e03"
+    "sec:orga586352"
+    "sec:org9fc6d30"
+    "prop2.6"
+    "sec:orgd64bfd9"
+    "sec:orgf90083d"
+    "sec:org11b6a5d"
+    "sec:org14f1fc1"
+    "prop3.2"
+    "sec:org0e69e5c"
+    "sec:orgf2262a8"
+    "sec:org71e5701"
+    "sec:org48949cd"
+    "eq4.1.1"
+    "eq4.1.1.2"
+    "example4.1.15"
+    "prop4.1.2"
+    "sec:orgcb36e8d"
+    "sec:org5a4ff66"
+    "prop4.3.6"
+    "sec:org50616a5"))
  :latex)
 
