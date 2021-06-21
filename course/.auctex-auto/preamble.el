432e82bfd7c927eed21379cb894b674bd7461cf1
wu <wu@wudeMacBook-Pro.local>
2021-06-21 12:01:15 +0800

    6.21.2021

--- course/.auctex-auto/preamble.el
+++ course/.auctex-auto/preamble.el
@@ -4,50 +4,53 @@
    (setq TeX-command-extra-options
          "-shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("xcolor" "dvipsnames") ("inputenc" "utf8") ("tcolorbox" "most") ("fontenc" "B1" "T1") ("appendix" "toc" "page")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("inputenc" "utf8") ("fontenc" "B1" "T1") ("xcolor" "dvipsnames") ("esvect" "f") ("tcolorbox" "most") ("appendix" "toc" "page")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
-    "xcolor"
-    "forest"
     "inputenc"
-    "mathtools"
-    "pgfplots"
-    "amsthm"
+    "fontenc"
+    "xcolor"
     "amsmath"
-    "commath"
+    "mathtools"
+    "amsthm"
+    "amsbsy"
     "amssymb"
     "mathrsfs"
-    "mathabx"
     "stmaryrd"
     "empheq"
-    "tikz"
-    "tikz-cd"
-    "tcolorbox"
-    "threeparttable"
+    "centernot"
     "scalerel"
     "stackengine"
     "stackrel"
-    "tabularx"
-    "dsfont"
-    "fontenc"
-    "enumitem"
+    "nicematrix"
+    "tensor"
+    "blkarray"
     "siunitx"
+    "esvect"
+    "unicode-math"
+    "wasysym"
+    "pgfplots"
+    "tikz"
+    "tikz-cd"
+    "tikz-qtree"
+    "tcolorbox"
+    "threeparttable"
+    "tabularx"
+    "enumitem"
     "subcaption"
     "caption"
     "auncial"
     "float"
     "fancyhdr"
-    "unicode-math"
     "ifthen"
     "xargs"
-    "tensor"
     "imakeidx"
     "hyperref"
     "soul"
@@ -60,9 +63,17 @@
     '("yRightarrow" ["argument"] 1)
     '("yleftarrow" ["argument"] 1)
     '("yrightarrow" ["argument"] 1)
+    '("ucorner" 1)
+    '("lcorner" 1)
+    '("ceil" 1)
+    '("floor" 1)
+    '("triplerightarrow" 1)
+    '("doublerightarrow" 2)
     '("bbar" 1)
     '("stcomp" 1)
     '("interior" 1)
+    '("ove" 1)
+    '("und" 1)
     '("rom" 1)
     '("trcl" 1)
     '("LRarrow" 1)
@@ -72,131 +83,40 @@
     '("wt" 1)
     '("Wt" 1)
     '("bl" 1)
+    '("norm" 1)
     '("arc" 1)
     "arc"
+    "abs"
     "widehatsym"
     "lowerwidehatsym"
     "cupdot"
     "bigcupdot"
-    "bx"
-    "bz"
-    "ba"
-    "bk"
-    "bs"
-    "bh"
-    "bc"
-    "br"
-    "bo"
-    "bU"
-    "bL"
-    "bV"
-    "bv"
-    "bp"
-    "bI"
-    "bM"
-    "bN"
-    "bK"
-    "bt"
-    "bb"
-    "bA"
-    "bX"
-    "bu"
-    "bS"
-    "bZ"
-    "by"
-    "bw"
-    "bT"
-    "bF"
-    "bm"
-    "bW"
-    "bR"
-    "bC"
-    "bD"
-    "bE"
-    "bQ"
-    "bP"
-    "bY"
-    "bH"
-    "bB"
-    "bG"
+    "rmodels"
     "bto"
     "Ra"
     "zfm"
-    "cl"
-    "sg"
-    "trdeg"
-    "cf"
-    "zfc"
-    "type"
-    "zf"
-    "im"
-    "inn"
-    "AC"
-    "cod"
-    "dom"
-    "ran"
-    "textd"
-    "td"
-    "id"
-    "LT"
-    "Mat"
-    "Eq"
-    "irr"
-    "Fr"
-    "Gal"
-    "lcm"
-    "alg"
-    "Th"
-    "DLO"
-    "DAG"
-    "ODAG"
     "varprod"
-    "Ab"
-    "Alg"
-    "Rng"
-    "Sets"
-    "Met"
-    "Aut"
-    "RMod"
-    "RAlg"
-    "LF"
-    "op"
-    "Diag"
-    "el"
-    "depth"
-    "FO"
-    "fin"
-    "qr"
-    "Mod"
-    "TC"
-    "Part"
-    "Tot"
-    "graph"
-    "Fin"
-    "Cof"
-    "lh"
-    "ord"
-    "Idem"
-    "zdiv"
-    "Frac"
-    "rad"
-    "nil"
-    "Ann"
-    "End"
-    "coim"
-    "coker"
-    "Bil"
-    "Tril"
     "bigtimes"
     "odiv"
     "subsetsim"
     "simsubset"
     "simsubsetsim"
-    "zallman")
+    "bigzero"
+    "rvline"
+    "zallman"
+    "elzevier"
+    "isoto"
+    "embto"
+    "oldabs")
    (LaTeX-add-environments
     '("claim" 1)
     "bsm"
     "psm")
+   (LaTeX-add-xcolor-definecolors
+    "ccqqqq")
+   (LaTeX-add-mathtools-DeclarePairedDelimiters
+    '("abss" "")
+    '("normm" ""))
    (LaTeX-add-amsthm-newtheorems
     "dummy"
     "definition"
