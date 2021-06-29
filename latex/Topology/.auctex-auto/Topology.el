329e036c532966b94031a383446cd37df53181a8
John Doe <f_dogs@protonmail.com>
2021-06-29 15:41:50 +0800

    topology

--- latex/Topology/.auctex-auto/Topology.el
+++ latex/Topology/.auctex-auto/Topology.el
@@ -7,13 +7,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,16 +34,16 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgbe1fae9"
-    "sec:orga5827aa"
-    "sec:org447db0b"
+    "sec:org9701fa1"
+    "sec:org83e84ab"
+    "sec:orga8fd64e"
     "lemma13.2"
-    "sec:org135dfd1"
-    "sec:org41146b6"
-    "sec:org41d6913"
+    "sec:org7351f7f"
+    "sec:org770797f"
+    "sec:org22fbe1b"
     "15.1"
     "ex15.7"
-    "sec:orgc43c492"
+    "sec:orge29a94b"
     "thm17.2"
     "thm17.5"
     "fig:17.3"
@@ -51,10 +51,10 @@
     "ex17.6"
     "ex17.8"
     "ex17.13"
-    "sec:org921dfb3"
+    "sec:org7eeaa76"
     "ex18.11"
     "ex18.12"
-    "sec:org7d4323e"
+    "sec:org1e6f1ff"
     "thm19.2"
     "thm19.3"
     "thm19.4"
@@ -62,8 +62,11 @@
     "thm19.6"
     "ex19.6"
     "ex19.7"
-    "sec:org3959bd9")
+    "sec:orged4ed79"
+    "ex20.4")
    (LaTeX-add-index-entries
-    "converge"))
+    "converge"
+    "uniform metric"
+    "uniform topology"))
  :latex)
 
