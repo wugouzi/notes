3ff9453a088c934810b1f2187d31e876b7653f36
John Doe <f_dogs@protonmail.com>
2021-07-01 22:32:02 +0800

    topology

---
+++ latex/TheoriesOfProgrammingLanguages/.auctex-auto/TheoriesOfProgrammingLanguages.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "TheoriesOfProgrammingLanguages"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "-shell-escape")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "preamble"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "syntax")
+   (LaTeX-add-labels
+    "sec:org908761b"
+    "sec:orgb8c8325"
+    "sec:org58931d8"
+    "sec:org59a89e1"))
+ :latex)
+
--- latex/Topology/.auctex-auto/Topology.el
+++ latex/Topology/.auctex-auto/Topology.el
@@ -7,13 +7,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,16 +34,16 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org0c95fcb"
-    "sec:orgaff712f"
-    "sec:org95c1ecd"
+    "sec:org3b4e52a"
+    "sec:org9a28a8e"
+    "sec:orgadccc67"
     "lemma13.2"
-    "sec:orgc805a27"
-    "sec:org9b6977a"
-    "sec:orgc83daee"
+    "sec:orgece4c0e"
+    "sec:orgc184347"
+    "sec:org8fa9482"
     "15.1"
     "ex15.7"
-    "sec:org3b0b29d"
+    "sec:org202bab0"
     "thm17.2"
     "thm17.5"
     "fig:17.3"
@@ -51,11 +51,11 @@
     "ex17.6"
     "ex17.8"
     "ex17.13"
-    "sec:orge3d5ea0"
+    "sec:org81e88d0"
     "thm18.4"
     "ex18.11"
     "ex18.12"
-    "sec:org465d5b3"
+    "sec:org9552938"
     "thm19.2"
     "thm19.3"
     "thm19.4"
@@ -63,24 +63,32 @@
     "thm19.6"
     "ex19.6"
     "ex19.7"
-    "sec:org711aa45"
+    "sec:org069fb1b"
     "ex20.4"
     "ex20.5"
-    "sec:orgdbb1a5c"
-    "sec:org1ad3c14"
-    "sec:org4a196fb"
+    "sec:org947e0fc"
+    "sec:org672bb61"
+    "sec:orgf82b4f0"
     "lemma23.2"
     "thm23.3"
     "ex23.12"
-    "sec:org539e530"
-    "sec:orgdda0ebf"
+    "sec:orgdef5639"
+    "sec:org57e35eb"
     "thm26.2"
     "thm26.3"
     "lemma26.4"
-    "lemma26.8")
+    "lemma26.8"
+    "thm26.9"
+    "ex26.1"
+    "ex26.2"
+    "ex26.3"
+    "ex26.4"
+    "sec:orga0c10a8"
+    "ex27.2")
    (LaTeX-add-index-entries
     "converge"
     "uniform metric"
-    "uniform topology"))
+    "uniform topology"
+    "compact"))
  :latex)
 
